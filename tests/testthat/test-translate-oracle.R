1aa9c314432daa93ea05b6e498e648f9da1e218a
Vitaly Druker <vdruker@gmail.com>
2019-05-03 11:35:12 -0400

    Added tests for oracle %in% operator

--- tests/testthat/test-translate-oracle.R
+++ tests/testthat/test-translate-oracle.R
@@ -20,3 +20,30 @@ test_that("queries translate correctly", {
     sql("^SELECT [*] FROM [(]SELECT [*]\nFROM [(]`df`[)] [)] `[^`]*` WHERE ROWNUM [<][=] 6")
   )
 })
+
+test_that("paste and paste0 translate correctly", {
+  trans <- function(x) {
+    translate_sql(!!enquo(x), con = simulate_oracle(), window = FALSE)
+  }
+
+  expect_equal(trans(paste(x, y)), sql("`x` || ' ' || `y`"))
+  expect_equal(trans(paste0(x, y)), sql("`x` || `y`"))
+})
+
+test_that("custom %in% is working correctly",{
+  library(stringr)
+  trans <- function(x, con = simulate_oracle()) {
+    translate_sql(!!enquo(x), con = con)
+  }
+  long_or <- trans(x %in% 1:1001)
+  normal_or <- trans(x %in% 1:1000)
+  normal_gen <- trans(x %in% 1:1000, con = simulate_odbc())
+
+  expect_equal(str_count(long_or, "OR"), 1)
+  expect_equal(str_count(long_or, "`x` IN"), 2)
+
+  expect_equal(normal_or, normal_gen)
+
+
+
+})
